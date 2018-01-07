8210cd0698b284f2abee2ba36c94322f4b60b966
Lev Stolpner <l.a.stolpner@gmail.com>
2018-01-07 21:27:25 +0300

    Ideas for improvements

--- src/ru/stolpner/Main.java
+++ src/ru/stolpner/Main.java
@@ -15,9 +15,8 @@ public class Main {
      *             following numbers - integers, representing available container capacities
      */
 
-    //TODO improve architecture, improve algorithm, java docs, namings
+    //TODO improve architecture, check for SOLID, namings, refactoring for readability
     //TODO separate "global" class with info on AMOUNT and CAPACITIES
-    //TODO refactoring
     public static void main(String[] args) {
         List<Integer> convertedArgs = MeasuringUtils.convertInput(args);
         Measurer.measure(convertedArgs.get(0), convertedArgs.get(1), convertedArgs.get(2));
